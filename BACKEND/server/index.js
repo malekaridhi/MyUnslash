377633becae71eed0c3ae11f5dc600a8fd5a337b
malekaridhi <malek.aridhii@gmail.com>
2021-01-19 14:44:38 +0100

    backFront

--- BACKEND/server/index.js
+++ BACKEND/server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose= require("mongoose");
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+require("dotenv").config();
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
